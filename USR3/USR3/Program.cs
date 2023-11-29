a64b9ccf956ac528db1c8f059f13a4072d1c87fc
snapp <3gotosnapp@gmail.com>
2023-11-29 19:54:53 +0300

    USR3 init

---
+++ USR3/USR3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace USR3
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] arr = new int[10] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            Console.WriteLine("Четные: {0}", arr.Count(i => i % 2 == 0));
+            Console.WriteLine("Нечетные: {0}", arr.Count(i => i % 2 == 1));
+        }
+    }
+}
